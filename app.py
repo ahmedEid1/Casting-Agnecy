c42ad12475ddf825bfde815eb2dbdcca3bed4395
ahmedEid1 <ahmedeid6422@gmail.com>
2021-07-14 14:34:59 +0200

    deployment

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
+import os
 from flask import Flask
+from models import setup_db
 
-app = Flask(__name__)
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    setup_db(app)
+    CORS(app)
+
+    @app.route('/')
+    def get_greeting():
+        excited = os.environ['EXCITED']
+        greeting = "Hello"
+        if excited == 'true': greeting = greeting + "!!!!!"
+        return greeting
+
+    @app.route('/coolkids')
+    def be_cool():
+        return "Be cool, man, be coooool! You're almost a FSND grad!"
+
+    return app
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+app = create_app()
 
 if __name__ == '__main__':
     app.run()
